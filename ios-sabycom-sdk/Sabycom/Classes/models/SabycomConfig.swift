dec8e9f1edc8ad58d8546b5ad1bb506d8454ec00
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-10-17 17:59:01 +0300

    new change

---
+++ ios-sabycom-sdk/Sabycom/Classes/models/SabycomConfig.swift
@@ -0,0 +1,13 @@
+//
+//  SabycomConfig.swift
+//  Sabycom
+//
+//  Created by Sergey Iskhakov on 19.08.2021.
+//
+
+import UIKit
+
+struct SabycomConfig {
+    let title: String
+    let headerColor: UIColor
+}
