40c397780de43cf298b1263b3682e2e32deebbaf
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-10-17 17:59:14 +0300

    new change

---
+++ ios-sabycom-sdk/Sabycom/Classes/api/responses/UnreadConversationCountResponse.swift
@@ -0,0 +1,12 @@
+//
+//  UnreadConversationCountResponse.swift
+//  Pods-ios-sabycom-demo
+//
+//  Created by Sergey Iskhakov on 23.08.2021.
+//
+
+import Foundation
+
+struct UnreadConversationCountResponse: Codable {
+    let count: Int
+}
