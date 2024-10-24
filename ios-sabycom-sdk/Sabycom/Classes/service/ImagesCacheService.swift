a1f87e9ae2c6e92538f118ee06ce27c1995be7b9
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-10-24 17:59:05 +0300

    new change

---
+++ ios-sabycom-sdk/Sabycom/Classes/service/ImagesCacheService.swift
@@ -0,0 +1,80 @@
+//
+//  ImagesCacheService.swift
+//  Sabycom
+//
+//  Created by Sergey Iskhakov on 12.11.2021.
+//
+
+import Foundation
+
+protocol ImagesCacheService {
+    func saveImage(_ image: UIImage, url: URL)
+    func loadImage(for url: URL) -> UIImage?
+}
+
+class ImagesCacheServiceImpl: ImagesCacheService {
+    private static let cachePath = "image-cache"
+
+    private static let cacheDirectoryURL: URL? = {
+        guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        return url.appendingPathComponent(cachePath)
+    }()
+    
+    func saveImage(_ image: UIImage, url: URL) {
+        guard let path = pathToImage(with: url)?.path else {
+            return
+        }
+
+        guard let data = image.pngData() else {
+            print("Не удалось создать файл картинки в формате png")
+            return
+        }
+
+        do {
+            try data.write(to: URL(fileURLWithPath: path), options: [.atomic, .completeFileProtectionUntilFirstUserAuthentication])
+        } catch {
+            print("Ошибка записи файла на диск по пути \(path)")
+        }
+    }
+
+    /// Загружает картинку по уникальному идентификатору из указанной папки
+    func loadImage(for url: URL) -> UIImage? {
+        guard let path = pathToImage(with: url)?.path else {
+            return nil
+        }
+
+        if !FileManager.default.fileExists(atPath: path) {
+            return nil
+        }
+
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: NSData.ReadingOptions.mappedIfSafe)
+            return UIImage(data: data)
+        } catch {
+            print("Ошибка загрузки файла с диска по пути: \(path)")
+        }
+
+        return nil
+    }
+    
+    private func pathToImage(with url: URL) -> URL? {
+        guard let cacheDirectoryURL = ImagesCacheServiceImpl.cacheDirectoryURL else {
+            return nil
+        }
+
+        if !FileManager.default.fileExists(atPath: cacheDirectoryURL.path) {
+            try? FileManager.default.createDirectory(at: cacheDirectoryURL, withIntermediateDirectories: true, attributes:nil)
+        }
+        
+        guard FileManager.default.fileExists(atPath: cacheDirectoryURL.path) else {
+            return nil
+            
+        }
+        
+        let imageName = url.dataRepresentation.base64EncodedString()
+        let imageUrl = cacheDirectoryURL.appendingPathComponent(imageName)
+        return imageUrl
+    }
+}
