ca5a4c8a0838ba0fd980ba2b39ba2628e434986f
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2026-09-05 17:59:10 +0300

    new change

---
+++ ios-sabycom-demo/Sources/ui/user/CreateUserInteractor.swift
@@ -0,0 +1,32 @@
+//
+//  CreateUserInteractor.swift
+//  ios-sabycom-demo
+//
+//  Created by Sergey Iskhakov on 08.10.2021.
+//  Copyright © 2021 Tensor. All rights reserved.
+//
+
+import Sabycom
+
+class CreateUserInteractor {
+    private let userStorage: UserStorage
+    
+    init(userStorage: UserStorage = DIContainer.shared.resolve(type: UserStorage.self)!) {
+        self.userStorage = userStorage
+    }
+    
+    func getCurrentUser() -> SabycomUser? {
+        return userStorage.currentUser
+    }
+    
+    func createOrUpdateUser(name: String, surname: String, email: String?, phone: String?) {
+        let userId = getCurrentUser()?.uuid ?? UUID().uuidString
+        
+        let user = SabycomUser(uuid: userId,
+                               name: name,
+                               surname: surname,
+                               email: email,
+                               phone: phone)
+        userStorage.saveUser(user)
+    }
+}
