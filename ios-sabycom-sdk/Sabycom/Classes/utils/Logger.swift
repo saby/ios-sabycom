06429a0315d0b8233b6397b97cc3ee216232fa6d
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2026-09-15 18:02:12 +0300

    new change

---
+++ ios-sabycom-sdk/Sabycom/Classes/utils/Logger.swift
@@ -0,0 +1,16 @@
+//
+//  Logger.swift
+//  Sabycom
+//
+//  Created by Sergey Iskhakov on 17.07.2026.
+//
+
+import Foundation
+
+let logger = Logger()
+
+class Logger {
+    func log(_ message: String) {
+        print(message)
+    }
+}
